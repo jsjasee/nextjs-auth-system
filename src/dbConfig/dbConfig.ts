ad16d1e042c82df15d283c04b706d3bc6ef8702e
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-03-15 22:17:30 +0800

    change event name in the dbconfig.ts code as it is a bug

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -7,8 +7,8 @@ export async function connect() {
     mongoose.connect(process.env.MONGO_URL!); // use ! to denote that it is always available..??? do we need to install dotenv here?? how do i get the .env file using file paths if im at dbConfig folder which is nested in src folder. and .env is in the same level as src folder? (outside src folder)??
     const connection = mongoose.connection;
 
-    connection.on("connection", () => {
-      console.log("MongoDB connected successfully");
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully!");
     }); // listen to the connection event?
 
     connection.on("error", (err) => {
